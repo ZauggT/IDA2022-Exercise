9eb7d310df18d241c31413333c4e704648b43ec9
Tim <tim.zaugg93@gmail.com>
2022-05-05 12:02:25 +0200

    image2

--- Button/assets/js/script.js
+++ Button/assets/js/script.js
@@ -1,9 +1,10 @@
 let n = 0; /*schlüsselwort let und variablenname. zuweisung des werts von rechts nach links */
+let y = 0;
+let x = 0;
 function buttonPressed() {
   n++;
   document.getElementById("element").style.left = n * 10 + "px";
 }
-console.log("hallo von extern");
 
 function neuesElement() {
   let div = document.createElement("div");
@@ -14,12 +15,22 @@ function neuesElement() {
 }
 function neuesBild() {
   n++;
-  let versatz = 20; /* um wieviel das bild jedesmal nach rechts rückt*/
+  x = x + 1;
+  let versatzX = 30; /* um wieviel das bild jedesmal nach rechts rückt*/
+  let versatzY = 40;
+
   let myImage = new Image(200, 100);
   myImage.src = "assets/images/people.png";
   myImage.classList.add("bild");
-  /* myImage.style.left = n * versatz + "px"; */
-  myImage.style.left = Math.random() * window.innerWidth + "px";
-  myImage.style.top = Math.random() * window.innerHeight + "px";
+  myImage.style.left = x * versatzX + "px";
+  myImage.style.top = y * versatzY + "px";
+
+  /*   myImage.style.left = Math.random() * window.innerWidth + "px";
+  myImage.style.top = Math.random() * window.innerHeight + "px"; */
   document.body.appendChild(myImage);
+  console.log(x, y);
+  if (n % 10 == 0) {
+    x = 0;
+    y = y + 1;
+  }
 }
