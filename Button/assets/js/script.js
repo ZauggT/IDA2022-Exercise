2513586a12d883fe558e42d50f0f58334b79470d
Tim <tim.zaugg93@gmail.com>
2022-05-05 11:10:38 +0200

    image

--- Button/assets/js/script.js
+++ Button/assets/js/script.js
@@ -1,7 +1,7 @@
 let n = 0; /*schlüsselwort let und variablenname. zuweisung des werts von rechts nach links */
 function buttonPressed() {
   n++;
-  document.getElementById("element").style.left = n + "px";
+  document.getElementById("element").style.left = n * 10 + "px";
 }
 console.log("hallo von extern");
 
@@ -11,4 +11,15 @@ function neuesElement() {
   div.style.left = Math.random() * window.innerWidth + "px";
   div.style.top = Math.random() * window.innerHeight + "px";
   document.body.appendChild(div);
+}
+function neuesBild() {
+  n++;
+  let versatz = 20; /* um wieviel das bild jedesmal nach rechts rückt*/
+  let myImage = new Image(200, 100);
+  myImage.src = "assets/images/people.png";
+  myImage.classList.add("bild");
+  /* myImage.style.left = n * versatz + "px"; */
+  myImage.style.left = Math.random() * window.innerWidth + "px";
+  myImage.style.top = Math.random() * window.innerHeight + "px";
+  document.body.appendChild(myImage);
 }
