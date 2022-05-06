e89bc9b63bffc0b0ebaf5f1703604e7f66c470c9
Tim <tim.zaugg93@gmail.com>
2022-05-06 10:18:44 +0200

    rename pamp.html

--- arrays/assets/js/script.js
+++ arrays/assets/js/script.js
@@ -9,3 +9,29 @@ console.log(array);
 for (let i = 0; i < array.length; i++) {
   console.log(array[i]);
 }
+
+let array2 = [];
+let num_items = 50;
+for (let i = 0; i < num_items; i++) {
+  let randomVal = Math.random() * 1000;
+  array2.push(randomVal);
+}
+console.log(array2);
+
+let object = {
+  x: 100,
+  y: 200,
+  width: 100,
+  height: 300,
+};
+
+let div = document.createElement("div");
+div.style.position = "fixed";
+div.style.width = object.width + "px";
+div.style.height = object.height + "px";
+div.style.top = object.y + "px";
+div.style.left = object.x + "px";
+div.style.backgroundColor = "#f33";
+
+document.body.appendChild(div);
+console.log(object);
