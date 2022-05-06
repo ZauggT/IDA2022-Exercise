9f48d89250e641121cc9ff54753cd12201048441
Tim <tim.zaugg93@gmail.com>
2022-05-06 09:45:26 +0200

    netlify links

---
+++ arrays/assets/js/script.js
@@ -0,0 +1,11 @@
+console.log("hello World");
+
+let number = 9;
+
+let array = [12, 4, 72, 2, 33, 21, 7, 93, 3, 44, 52];
+
+console.log(array);
+
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
