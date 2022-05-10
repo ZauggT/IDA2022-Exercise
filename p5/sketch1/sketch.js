33278f53338a69a67780a8d5682ac23d2be6e0a5
Tim <tim.zaugg93@gmail.com>
2022-05-10 10:27:55 +0200

    p5

--- p5/sketch1/sketch.js
+++ p5/sketch1/sketch.js
@@ -13,20 +13,46 @@ function setup() {
 }
 
 function draw() {
-  background(2, 204, 123, 10);
+  /*   background(2, 204, 123, 10);
   fill(random(255), random(255), random(255));
   ellipse(mouseX, mouseY, random(1, 20));
-  line(width / 2, height / 2, mouseX, mouseY);
+  line(width / 2, height / 2, mouseX, mouseY); */
 
-  background(0);
+  background(0, 100);
+  fill(random(255), random(255), random(255));
+  /*   textSize(map(history[count].visitCount, 1, 100, 8, 50));
   text(history[count].title, width / 2, height / 2);
   count++;
   if (count > Object.keys(history).length) {
     count = 0;
+  } */
+  let start = 0;
+  let zeile = 20; /* Versatz in der y Achse, soviel rutscht jeder Eintrag gegen unten*/
+  let step = 2; /* Um soviel Pixel wird der nächste Eintrag grösser */
+  let portion = 50;
+  for (let i = start; i < portion + start; i++) {
+    textSize((i - start) * step);
+    text(history[i].title, width / 2, (i - start - 1) * zeile);
   }
 }
 
-function mouseClicked() {
-  clear();
-  redraw();
+function keyReleased() {
+  if (key == "s" || key == "S") {
+    let d = new Date();
+    let now =
+      d.getFullYear() +
+      "" +
+      (d.getMonth() + 1) +
+      "" +
+      d.getDate() +
+      "" +
+      (d.getHours() + 1) +
+      "-" +
+      (d.getMinutes() + 1) +
+      "" +
+      (d.getSeconds() + 1) +
+      "-" +
+      frameCount;
+    saveCanvas(now, "png");
+  }
 }
